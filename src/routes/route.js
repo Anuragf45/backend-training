d2d2012abf826521c6a4a097c4e10b3a2b4a0051
Anurag <anuragf45@gmail.com>
2022-08-08 22:49:52 +0530

    Assignment 8aug

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { toNumber } = require('lodash');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
@@ -32,7 +33,110 @@ router.get('/student-details/:name', function(req, res){
     let studentName = requestParams.name
     console.log('Name of the student is ', studentName)
     
-    res.send('Dummy response')
+    res.send('The name is '+ studentName)
 })
 
+
+router.get('/student-details:name',function(req,res){
+    const students=["anurag",'Aman'];
+    console.log("This is the name " + JSON.stringify(req.params))
+    let reqParams=req.params;
+    let studentName=reqParams.name;
+
+    res.send(studentName + " Srivastava")
+})
+// Problem 1
+router.get('/GET/movies',function(req,res){
+   const movies= ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
+  
+   res.send(movies)
+})
+
+// router.get('/GET/movies/:indexNumber',function(req,res){
+//     const movies= ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
+//     let a=req.params;
+//     let b=Object.values(a)
+//     for(let i=0;i<=b.length-1;i++){
+//         console.log(b[i]);
+//         let c=b[i];
+//         if(c<=movies.length-1){
+//             res.send(res.send(movies[c]))
+//         }else{
+//             res.send("Enter valid index")
+//         }
+//     }
+//     })
+
+// Problem 2
+router.get('/GET/movies/:indexNumber',function(req,res){
+    const movies= ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"];
+    let reqParams=req.params;
+    let index=reqParams.indexNumber;
+    if(index>=0 && index<=movies.length-1){
+        res.send(movies[index])
+    }else{
+        res.send("Wrong index");
+    }
+
+})
+
+// Problem 4
+    router.get('/GET/films',function(req,res){
+        const moviess=[{
+            id: 1,
+            name: "The Shining"
+           }, {
+            id: 2,
+            name:" Incendies"
+           }, {
+            id: 3,
+            name: "Rang de Basanti"
+           }, {
+            id: 4,
+            name: "Finding Nemo"
+           }]
+           res.send(moviess)
+
+    })
+
+
+
+
+// Problem 5
+
+router.get("/GET/films/:filmId", function (req, res) {
+    let films = [
+      {
+        id: 1,
+        name: "The Shining",
+      },
+      {
+        id: 2,
+        name: "Incendies",
+      },
+      {
+        id: 3,
+        name: "Rang de Basanti",
+      },
+      {
+        id: 4,
+        name: "Finding Nemo",
+      },
+    ];
+  
+    let requestParams = req.params;
+    const filmid = parseInt(requestParams.filmId);
+    const fimlLength = films.length;
+  
+    let result = "";
+    for (let i = 0; i < fimlLength; i++) {
+      if (filmid === films[i].id) {
+        result = films[i];
+        break;
+      } else {
+        result = "No movie exists with this id";
+      }
+    }
+    res.send(result);
+  });
 module.exports = router;
