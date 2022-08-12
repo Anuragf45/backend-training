7103ddf3a169f9162bab4b2fc8473206ff2e7162
Anurag <anuragf45@gmail.com>
2022-08-12 14:49:05 +0530

    mongo changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-
+const {default:mongoose}=require('mongoose')
 const route = require('./routes/route.js');
 
 const app = express();
@@ -8,6 +8,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb+srv://user_1:password45@cluster0.ocjw7tu.mongodb.net/?retryWrites=true&w=majority",{
+    useNewUrlParser:true
+})
+.then(   ()=>console.log("MongoDB is connected"))
+.catch(  err=>console.log(err))
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
