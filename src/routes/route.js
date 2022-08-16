d1ecace14a50bab19814aa8906b0497a5228bedd
Anurag <anuragf45@gmail.com>
2022-08-16 17:50:09 +0530

    recovery

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createUser } = require('../controllers/userController');
 const router = express.Router();
 const Usercontroller=require('../controllers/userController');
 const bookController=require('../controllers/bookController');
 const studentController=require('../controllers/studentController');
-
+const labourController=require('../controllers/labourController');
+const playerController=require('../controllers/playerController');
+const employeeController=require('../controllers/employeeController');
+const groceryController=require('../controllers/groceryController')
 
 router.post('/test-me', function (req, res) {
     
@@ -24,5 +26,17 @@ router.get('/getBookData', bookController.getBookData);
 router.post('/createStudent',studentController.createStudent)
 
 
+router.post('/labourDetails1',labourController.createLabour )
+router.get('/labourDetails2',labourController.labourData);
+
+
+router.post('/playerDetails',playerController.createPlayer);
+
+
+router.post('/createEmployee',employeeController.createEmployee);
+router.get('/getEmployee',employeeController.getEmployee);
+
+router.post('/grocery',groceryController.createData )
+router.get('/groData',groceryController.getData);
 module.exports = router;
 
