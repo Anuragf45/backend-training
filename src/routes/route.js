13e082edf7e81e161856e0ae83a82fc24b4c8c85
Anurag <anuragf45@gmail.com>
2022-08-28 01:01:38 +0530

    route update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const router = express.Router();
-// const controller=require('../controllers/userController')
-// const middleware=require('../middleware/mid1')
-const productController=require('../controllers/productController')
-const userController=require('../controllers/userController')
-const orderController=require('../controllers/orderController');
-const { mid1, mid2, mid3 } = require('../middleware/mid1');
+const myUserController=require('../controllers/myUserController');
+const middleware=require('../middleware/auth1')
 
+router.post('/createUser',myUserController.createData)
+router.post('/loginUser',myUserController.login);
 
-
-// router.get('/test-me', middleware.mid1, controller.a );
-
-router.post('/createProduct',productController.createProduct)
-router.post('/createUser',userController.createUser)
-router.post('/createOrder',mid1,mid2,orderController.createOrder)
-router.get('/get',orderController.find)
-
+router.get('/user/:userId',middleware.mid1,myUserController.getUserData)
+router.put('/user/:userId',middleware.mid1,myUserController.updateUser);
+router.delete('/delete/:userId',middleware.mid1,myUserController.deleteUser)
 
 
 module.exports = router;
